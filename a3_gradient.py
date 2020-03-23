a3 gradient added accuracy, a3 mod fixed some bugs
a3 mod needs more work

a3_gradient.py
@@ -14,6 +14,8 @@ def GD(x_train, y_train, x_test, y_test, th0, learning_rate, num_iter, SGD=False
     iters = np.array([])
     # "test" (validation) data losses
     losses_test = np.array([])
+    # testing accuracies
+    accuracies = np.array([])
 
     while num_iter>0:
         # find gradient gk
@@ -35,12 +37,14 @@ def GD(x_train, y_train, x_test, y_test, th0, learning_rate, num_iter, SGD=False
         # record loss
         losses = np.append(losses,f_thkp1)
         losses_test = np.append(losses_test, _f(x_test, y_test, thk))
+        y_pred_test = _sigmoid(thk, x_test)
+        accuracies = np.append(accuracies, _accuracy(y_test, y_pred_test))
         iters = np.append(iters, k)
         # calculate stopping condition
         k += 1
         num_iter -= 1
-    #return thk, losses, losses_test, iters
-    return thk, losses, losses_test, iters
+
+    return thk, losses, losses_test, accuracies, iters
 
 def _sigmoid (th, x):
     # print("in")
@@ -90,6 +94,14 @@ def _line_search(x, y, a_bar, gk, pk, thk, m1 = 1e-4, r=0.5):
         f_thk_apk = _f(x, y, thk + a * pk)
     return a
 
+def _accuracy(y,y_pred):
+    y_pred=np.rint(y_pred)
+    result = 0
+    for i in range(y_pred.shape[0]):
+        if y[i]==y_pred[i]:
+            result+=1
+    return result*1.0/y_pred.shape[0]
+
 def _plot2(x,y1,y2,legend1,legend2,x_label,y_label,title):
     line1,line2 = plt.plot(x,y1,x,y2)
     plt.legend((line1,line2),(legend1,legend2))
@@ -122,8 +134,11 @@ if __name__ == '__main__':
     # print(losses.shape)
     # print(losses_t.shape)
     # _plot2(iters, losses, losses_t, "Training loss", "Testing loss","Iteration #", "Loss", "Loss vs Iterations")
-    weights, losses, losses_t, iters = GD(x_train, y_train, x_test, y_test, th0, learning_rate=0.01, num_iter=500, SGD=True)
+    weights, losses, losses_t, accuracies, iters = GD(x_train, y_train, x_test, y_test, th0, learning_rate=0.01, num_iter=500, SGD=False)
     #print(losses)
     #plt.plot(iters, losses)
     _plot2(iters, losses, losses_t, "Training loss", "Testing loss", "Iteration #", "Loss", "Loss vs Iterations")
+    print(losses[losses.shape[0]-1])
+    print(losses_t[losses_t.shape[0]-1])
+    print(accuracies[accuracies.shape[0]-1])
     plt.show()

a3_mod.py
@@ -1,6 +1,7 @@
 import autograd.numpy as np
 from autograd import value_and_grad
-
+from data_utils import load_dataset
+import matplotlib.pyplot as plt
 
 def forward_pass(W1, W2, W3, b1, b2, b3, x):
     """
@@ -30,27 +31,32 @@ def forward_pass(W1, W2, W3, b1, b2, b3, x):
     # den = np.sum(temp)
     # Fhat = temp/den
     Fhat = _log_softmax(H2, W3, b3)
-    print("layer 1")
-    print(x.shape)
-    print(W1.shape)
-    print(b1.shape)
-    print("layer 2")
-    print(H1.shape)
-    print(W2.shape)
-    print(b2.shape)
-    print("layer 3")
-    print(H2.shape)
-    print(W3.shape)
-    print(b3.shape)
+    # print("layer 1")
+    # print(x.shape)
+    # print(W1.shape)
+    # print(b1.shape)
+    # print("layer 2")
+    # print(H1.shape)
+    # print(W2.shape)
+    # print(b2.shape)
+    # print("layer 3")
+    # print(H2.shape)
+    # print(W3.shape)
+    # print(b3.shape)
 
     return Fhat
 
+def _softmax(x,w,b):
+    z = np.dot(x, w.T) + b.T
+    temp = np.exp(z)
+    den = np.sum(temp)
+    return z/den
 
 def _log_softmax(x,w,b):
     z = np.dot(x, w.T) + b.T
     temp = np.exp(z)
     den = np.sum(temp)
-    return z+np.log(den)
+    return z-np.log(den)
 
 def negative_log_likelihood(W1, W2, W3, b1, b2, b3, x, y):
     """
@@ -69,23 +75,20 @@ def negative_log_likelihood(W1, W2, W3, b1, b2, b3, x, y):
     # NLL of Gaussian
     # nll = 0.5*np.sum(np.square(Fhat - y)) + 0.5*y.size*np.log(2.*np.pi)
     # print(nll)
-    # NLL of Bernoulli
-    first = np.dot(y.T,(np.log(Fhat)))
-    print("shapes")
-    print(first)
-    print(y.shape)
-    print("Fhat")
-    print(Fhat.shape)
-    print(Fhat)
-    # print(first)
-    print("Fhat2")
-    print(np.ones((Fhat.shape[0],Fhat.shape[1]))-Fhat)
-    second = np.dot((np.ones((y.shape[0],y.shape[1]))-y).T,(np.log(np.ones((Fhat.shape[0],Fhat.shape[1]))-Fhat)))
-    print(second)
-    nll = first + second
-    print(nll)
+    # NLL of softmax
 
-    return nll
+    #print(Fhat)
+    #print(np.dot(y.T,np.log(Fhat)))
+    temp_vector = np.array([])
+    #print(Fhat)
+    for row in range(Fhat.shape[0]):
+        #print(y[row].shape)
+        temp_vector = np.append(temp_vector, np.dot(Fhat[row,:],y[row,:]))
+    # print(temp_vector.shape)
+    # temp = np.dot(np.log(Fhat),y.T)
+    # print(temp.shape)
+    # return -1.*np.sum(np.dot(np.log(Fhat),y.T))
+    return -1.*np.sum(temp_vector)
     
 
 nll_gradients = value_and_grad(negative_log_likelihood, argnum=[0,1,2,3,4,5])
@@ -102,7 +105,7 @@ nll_gradients = value_and_grad(negative_log_likelihood, argnum=[0,1,2,3,4,5])
         b1_grad : (M, 1) gradient of the nll with respect to the biases of first (hidden) layer
         b2_grad : (M, 1) gradient of the nll with respect to the biases of second (hidden) layer
         b3_grad : (10, 1) gradient of the nll with respect to the biases of third (output) layer
-     """
+ """
 
     
 def run_example():
@@ -113,7 +116,6 @@ def run_example():
     weights and biases to zero.
     """
     # load the MNIST_small dataset
-    from data_utils import load_dataset
     x_train, x_valid, x_test, y_train, y_valid, y_test = load_dataset('mnist_small')
     
     # initialize the weights and biases of the network
@@ -135,5 +137,62 @@ def run_example():
     print("negative log likelihood: %.5f" % nll)
 
 
+def sgd(x_train, y_train, x_valid, y_valid, x_test, y_test, layer_size=100, batch_size=250, learning_rate=0.001, num_iters=100):
+    """
+
+    :param x_train:
+    :param y_train:
+    :param x_valid:
+    :param y_valid:
+    :param x_test:
+    :param y_test:
+    :param layer_size:
+    :param batch_size:
+    :param learning_rate: learning rate for SGD
+    :param num_iters: number of iterations before stopping
+    :return:
+    """
+    iter_count = 0
+
+    W1 = np.zeros((layer_size, 784))  # weights of first (hidden) layer
+    W2 = np.zeros((layer_size, layer_size))  # weights of second (hidden) layer
+    W3 = np.zeros((10, layer_size))  # weights of third (output) layer
+    b1 = np.zeros((layer_size, 1))  # biases of first (hidden) layer
+    b2 = np.zeros((layer_size, 1))  # biases of second (hidden) layer
+    b3 = np.zeros((10, 1))  # biases of third (output) layer
+
+    losses_train = np.array([])
+    losses_valid = np.array([])
+    iters = np.array([])
+
+    while num_iters > 0:
+        iter_count += 1
+        num_iters -= 1
+
+        (nll, (W1_grad, W2_grad, W3_grad, b1_grad, b2_grad, b3_grad)) = \
+            nll_gradients(W1, W2, W3, b1, b2, b3, x_train[:batch_size], y_train[:batch_size])
+        print("negative log likelihood: %.5f" % nll)
+        # record training loss
+        losses_train = np.append(losses_train, nll)
+        # find validation loss
+        loss_valid = negative_log_likelihood(W1, W2, W3, b1, b2, b3, x_valid, y_valid)
+        losses_valid = np.append(losses_valid, loss_valid)
+        # assume that gradients averaged already i.e. 1/|B|
+        W1 -= learning_rate*W1_grad
+        W2 -= learning_rate*W2_grad
+        W3 -= learning_rate*W3_grad
+        b1 -= learning_rate*b1_grad
+        b2 -= learning_rate*b2_grad
+        b3 -= learning_rate*b3_grad
+
+        # record iter number
+        iters = np.append(iters, iter_count)
+
+    plt.plot(iters, losses_valid)
+    plt.show()
+
+
 if __name__ == '__main__':
     run_example()
+    x_train, x_valid, x_test, y_train, y_valid, y_test = load_dataset('mnist_small')
+    sgd(x_train, y_train, x_valid, y_valid, x_test, y_test)

a3_testfile.py
@@ -1,2 +1,16 @@
+import autograd.numpy as np   # Thinly-wrapped version of Numpy
+from autograd import grad
+
+
+def taylor_sine(x):  # Taylor approximation to sine function
+    ans = currterm = x
+    i = 0
+    while np.abs(currterm) > 0.001:
+        currterm = -currterm * x**2 / ((2 * i + 3) * (2 * i + 2))
+        ans = ans + currterm
+        i += 1
+    return ans
+
+
 if __name__ == '__main__':
-    pass
+    print(taylor_sine(10))